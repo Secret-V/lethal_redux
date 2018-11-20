effd40fa143d3a86ee58dd37ed66a53729846c9a
Vincent Booman <vincent_booman@msn.com>
2018-11-20 15:25:48 +0100

    Inital project upload.

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    public float startSpeed;
+    [HideInInspector]
+    public float speed;
+    [HideInInspector]
+    public Vector3 direction;
+    [HideInInspector]
+    public int lastHitter = -1;
+    [HideInInspector]
+    public Vector3 position;
+
+    //more behavoir types of the ball can be added here
+    public enum BallState
+    {
+        SERVE,
+        NORMAL,
+        HITSTUN,
+        STANDBY
+    }
+    [HideInInspector]
+    public BallState ballState = BallState.SERVE;
+    private Vector3 startPosition;
+
+    public static BallScript ball;
+
+    void Start () {
+        ball = this;
+        speed = startSpeed;
+        position = startPosition = transform.position;
+	}
+
+    //updating the state of the ball
+    void Update() {
+
+        //NORMAL is the state where the ball moves
+        if (ballState == BallState.NORMAL)
+        {
+            position += direction * speed;
+        }
+
+        //if the ball is not dead, we have to make sure it stays in the stage
+        if (ballState != BallState.STANDBY)
+        {
+            if (position.x < -9)
+            {
+                HitWall(true);
+                position.x = -9;
+            }
+            if (position.x > 9)
+            {
+                HitWall(true);
+                position.x = 9;
+            }
+            if (position.y < -5)
+            {
+                HitWall(false);
+                position.y = -5;
+            }
+            if (position.y > 5)
+            {
+                HitWall(false);
+                position.y = 5;
+            }
+        }
+
+        transform.position = position;
+    }
+
+    //the ball gets hit
+    public void GetHit(PlayerScript byPlayer, float hitPause)
+    {
+        //you can set the starting speed in the editor
+        //we can play around with this of course
+        //try speed *= 1.1f instead
+        speed = 0.3f;
+        lastHitter = byPlayer.playerNum;
+
+        //default direction set to which player it is
+        if (byPlayer.playerNum == 0) direction = Vector2.right;
+        else direction = Vector2.left;
+        StartCoroutine(GetHitCoroutine(hitPause));
+    }
+
+    public void HitWall(bool vertical)
+    {
+        if (vertical) direction.x *= -1;
+        else direction.y *= -1;
+        lastHitter = -1;
+    }
+
+    public IEnumerator GetHitCoroutine(float hitPause)
+    {
+        ballState = BallState.HITSTUN;
+
+        yield return new WaitForSeconds(hitPause);
+
+        ballState = BallState.NORMAL;
+    }
+
+    public IEnumerator HitPlayerCoroutine(PlayerScript player)
+    {
+        ballState = BallState.STANDBY;
+        position = 1000 * Vector3.down;
+        speed = startSpeed;
+
+        yield return new WaitForSeconds(2.4f);
+
+        lastHitter = -1;
+        ballState = BallState.SERVE;
+        position = player.position + Vector3.right*(player.playerNum == 0 ? 1.5f : -1.5f);
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    //which player it is. 0 is left, 1 is right
+    public int playerNum;
+
+    //these can be tweaked in the editor
+    public float moveSpeed;
+    public float swingDuration;
+    public float recoveryDuration;
+
+    //you can add stuff here and just drag a sprite on it in the editor
+    //same for the ball, if you want a different visual there
+    //spriteRenderer.sprite = whateverSprite; is how you change picture
+    private SpriteRenderer spriteRenderer;
+    public Collider swingCollider;
+    public Sprite idleSprite;
+    public Sprite swingSprite;
+    public Sprite hitSprite;
+    public Sprite recoverySprite;
+    public Sprite dieSprite;
+
+    //input mapping (just set these in the editor)
+    public KeyCode hitKey = KeyCode.C;
+    public KeyCode upKey = KeyCode.W;
+    public KeyCode downKey = KeyCode.S;
+    public KeyCode rightKey = KeyCode.D;
+    public KeyCode leftKey = KeyCode.A;
+
+    [HideInInspector]
+    public Vector3 position;
+
+    //if you want to add more abilities or states of the player, add them in here
+    //and then put them in Update() so the game knows what to do during them
+    public enum PlayerState
+    {
+        NORMAL,
+        SWINGING,
+        HITTING,
+        SWING_RECOVERY,
+        GET_HIT,
+        DEAD,
+    }
+    PlayerState playerState = PlayerState.NORMAL;
+    Vector3 startPosition;
+
+    void Start()
+    {
+        position = startPosition = transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        swingCollider.enabled = false;
+    }
+
+    //checking if we hit the ball
+    public void OnTriggerStay(Collider otherCollider)
+    {
+        BallScript ball = otherCollider.GetComponent<BallScript>();
+        if (ball.lastHitter != playerNum) //don't hit the ball twice
+        {
+            //you can do exemptions or calculations with this depending on the ball or the player
+            float hitPause = 0.1f;
+            
+            ball.GetHit(this, hitPause);//send info to the ball
+            StopAllCoroutines(); //stop the swinging 
+            StartCoroutine(HitCoroutine(hitPause)); //start the hitting with the right hitpause duration
+        }
+    }
+
+    //checking if we get hit
+    public void OnCollisionEnter(Collision collision)
+    {
+        BallScript ball = collision.rigidbody.GetComponent<BallScript>();
+
+        //only if the ball is flying around, you could put other exemptions here
+        if (ball != null && ball.ballState == BallScript.BallState.NORMAL) 
+        {
+            StopAllCoroutines(); //stop whatever you're doing
+            StartCoroutine(DieCoroutine()); //start dying
+            ball.StartCoroutine(ball.HitPlayerCoroutine(this)); //remove the ball but let it know who it killed
+        }
+    }
+
+    //we update the state of the player
+    //what sprite to show and what hitbox to enable
+    //you can add more abilities here as PlayerStates, more or other hitboxes etc
+    void Update()
+    {
+        switch (playerState)
+        {
+            case PlayerState.NORMAL:
+                swingCollider.enabled = false;
+                spriteRenderer.sprite = idleSprite;
+
+                //this is where we can check to start actions
+                //since the player is doing nothing right now
+                if (Input.GetKeyDown(hitKey))
+                {
+                    StartCoroutine(SwingCoroutine());
+                }
+
+                //move around
+                if (Input.GetKey(rightKey))
+                    position += Vector3.right * moveSpeed;
+                if (Input.GetKey(leftKey))
+                    position += Vector3.left * moveSpeed;
+
+                position.x = Mathf.Clamp(position.x, -8f, 8f);
+                break;
+
+            case PlayerState.SWINGING:
+                swingCollider.enabled = true;
+                spriteRenderer.sprite = swingSprite;
+                break;
+
+            case PlayerState.HITTING:
+                swingCollider.enabled = true;
+                spriteRenderer.sprite = hitSprite;
+                //as we're hitting, we can aim the ball
+                //feel free to remove or alter this completely
+
+                //for setting specific directions you can use: 
+                //new Vector2(0.5f, 0.5f); is diagonally up for example
+                //see it as distance from zero, the direction is the line between zero and your x y numbers
+                //Vector3.up or down etc also works
+
+                if (playerNum == 0)//left side player
+                {
+                    if (Input.GetKey(rightKey))
+                        BallScript.ball.direction = Vector3.right;
+                    if (Input.GetKey(upKey))
+                        BallScript.ball.direction = new Vector2(0.5f, 0.5f);
+                    if (Input.GetKey(downKey))
+                        BallScript.ball.direction = new Vector2(0.5f, -0.5f);
+                }
+                else
+                {
+                    if (Input.GetKey(leftKey))
+                        BallScript.ball.direction = Vector3.left;
+                    if (Input.GetKey(upKey))
+                        BallScript.ball.direction = new Vector2(-0.5f, 0.5f);
+                    if (Input.GetKey(downKey))
+                        BallScript.ball.direction = new Vector2(-0.5f, -0.5f);
+                }
+                //tip: you can use BallScript.ball to access the ball from anywhere
+                break;
+            case PlayerState.SWING_RECOVERY:
+                swingCollider.enabled = false;
+                spriteRenderer.sprite = recoverySprite;
+                break;
+
+            case PlayerState.GET_HIT:
+                swingCollider.enabled = false;
+                spriteRenderer.sprite = dieSprite;
+                break;
+
+            case PlayerState.DEAD:
+                swingCollider.enabled = false;
+                spriteRenderer.sprite = dieSprite;
+                position += Vector3.down * 0.25f;
+                break;
+        }
+
+        transform.position = position;
+    }
+
+    //these are for doing the timings
+    //after WaitForSeconds it goes on where it left off
+    //if you want to add different abilities you can copy one over and change it
+
+    public IEnumerator SwingCoroutine()
+    {
+        playerState = PlayerState.SWINGING;
+        yield return new WaitForSeconds(swingDuration);
+
+        playerState = PlayerState.SWING_RECOVERY;
+        yield return new WaitForSeconds(recoveryDuration);
+
+        playerState = PlayerState.NORMAL;
+    }
+
+    public IEnumerator HitCoroutine(float hitPause)
+    {
+        playerState = PlayerState.HITTING;
+        yield return new WaitForSeconds(hitPause);
+
+        playerState = PlayerState.SWING_RECOVERY;
+        yield return new WaitForSeconds(recoveryDuration);
+
+        playerState = PlayerState.NORMAL;
+    }
+
+    public IEnumerator DieCoroutine()
+    {
+        playerState = PlayerState.GET_HIT;
+        yield return new WaitForSeconds(0.2f);
+
+        playerState = PlayerState.DEAD;
+        yield return new WaitForSeconds(2.0f);
+
+        playerState = PlayerState.NORMAL;
+        position = startPosition;
+    }
+}
